dd2bd2b520197a3bf08687a69115ac22ce58a4e6
Rootshell <bcairns@voidrealms.com>
2017-12-21 13:42:17 -0500

    first commit

---
+++ checkdemo/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => new MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+
+  bool _isChecked = false;
+
+  void onChanged(bool value) {
+    setState((){
+      _isChecked = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Checkbox Demo'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child:
+          new Column(
+            children: <Widget>[
+              new Row(
+                children: <Widget>[
+                  new Text('Click it ->'),
+                  new Checkbox(value: _isChecked, onChanged: (bool value){onChanged(value);}),
+                ],
+              ),
+              new CheckboxListTile(
+                  title: new Text('Click me!!!'),
+                  value: _isChecked,
+                  activeColor: Colors.red,
+                  secondary: const Icon(Icons.home),
+                  onChanged: (bool value){onChanged(value);}
+                  )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,11 @@
+
+Code for video tutorial
+
+Title: Flutter 29 - CheckBox
+Published: 12/21/2017 1:40 PM
+Link: https://youtu.be/-BWNtu917qQ
+
+Description: Learn how to use the Checkbox
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
